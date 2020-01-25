c0cffae914e086746c069866cb80f52b203e210a
coolg <kevinjrliang@gmail.com>
2020-01-25 17:19:41 -0500

    added one line

--- post.py
+++ post.py
@@ -1,11 +1,7 @@
-print('Hello World')
-
-print('Hola Mundo')
-print('helo kevin')
-
-print('aa')
-
 import cv2
+import os
+import numpy as np
+from pathlib import Path
 
 """
 img = cv2.imread('week_1_page_1.jpg')
@@ -13,6 +9,8 @@ text = tess.image_to_string(img)
 
 print(text)
 """
+location = r'small'
+
 
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
@@ -30,34 +28,35 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     return cv2.resize(image, dim, interpolation=inter)
 
 
+for f in os.listdir(location):
+    image = cv2.imread(os.path.join(location, f))
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (9, 9), 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY_INV,11,30)
 
-image = cv2.imread('week_1_page_1.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (9,9), 0)
-thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,30)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+    dilate = cv2.dilate(thresh, kernel, iterations=12)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
-dilate = cv2.dilate(thresh, kernel, iterations=8)
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    ROI_number = 0
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area > 10000:
+            x,y,w,h = cv2.boundingRect(c)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
+            if area > 160000:
+                ROI = image[y:y+h, x:x+w]
+                cv2.imwrite('ROI_{}.png'.format(ROI_number), ROI)
+                ROI_number += 1
 
-ROI_number = 0
-for c in cnts:
-    area = cv2.contourArea(c)
-    if area > 10000:
-        x,y,w,h = cv2.boundingRect(c)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
-        # ROI = image[y:y+h, x:x+w]
-        # cv2.imwrite('ROI_{}.png'.format(ROI_number), ROI)
-        # ROI_number += 1
+    resize1 = ResizeWithAspectRatio(thresh, width=880)
+    resize2 = ResizeWithAspectRatio(dilate, width=880)
+    resize = ResizeWithAspectRatio(image, width=880)
 
-
-resize1 = ResizeWithAspectRatio(thresh, width=880)
-resize2 = ResizeWithAspectRatio(dilate, width=880)
-resize = ResizeWithAspectRatio(image, width=880)
-
-cv2.imshow('thresh', resize1)
-cv2.imshow('dilate', resize2)
-cv2.imshow('image', resize)
-cv2.waitKey()
+    #cv2.imshow('thresh', resize1)
+    #cv2.imshow('dilate', resize2)
+    cv2.imshow('image', resize)
+    cv2.waitKey()
